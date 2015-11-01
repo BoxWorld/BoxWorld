63389e4a37652ac1367f60145fad89daa5f6a000
Han Maokun <hanmaokun@gmail.com>
2015-11-01 09:50:02 +0800

    Customized GL version(4, 0) settings at begginning.

--- src/App/main.cpp
+++ src/App/main.cpp
@@ -6,6 +6,20 @@
 //========================================================================
 
 int main( ){
-	ofSetupOpenGL(BOXWORLD_WIDTH, BOXWORLD_HEIGHT, OF_WINDOW);
+    
+    /* Customized gl version(4, 0) settings. */
+#ifdef TARGET_OPENGLES
+    ofGLESWindowSettings settings;
+    settings.glesVersion = 1;
+#else
+    ofGLWindowSettings settings;
+    settings.glVersionMajor = 4;
+    settings.glVersionMinor = 0;
+#endif
+    settings.width = BOXWORLD_WIDTH;
+    settings.height = BOXWORLD_HEIGHT;
+    settings.windowMode = OF_WINDOW;
+    ofCreateWindow(settings);
+    
 	ofRunApp(new BoxWorldApp());
 }
